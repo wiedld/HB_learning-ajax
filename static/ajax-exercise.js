09b6746606f645b5dc395ecb56f845f08cd5265c
Hackbright Student <info@hackbrightacademy.com>
2015-02-13 16:32:34 -0800

    basic ajax

--- static/ajax-exercise.js
+++ static/ajax-exercise.js
@@ -1,7 +1,9 @@
 // PART 1: SHOW A FORTUNE
 
 function showFortune(evt) {
-    // TODO: get the fortune and show it in the fortune-text div
+
+    $("#fortune-text").load("/fortune");
+
 }
 
 $('#get-fortune-button').on('click', showFortune);
@@ -11,11 +13,14 @@ $('#get-fortune-button').on('click', showFortune);
 
 
 // PART 2: SHOW WEATHER
-
+ 
 function showWeather(evt) {
     evt.preventDefault();
     var url = "/weather?zipcode=" + $("#zipcode-field").val();
     // TODO: request weather with that URL and show the forecast in #weather-info
+    $.get(url, function (result){
+        $("#weather-info").text(result['forecast']);
+    });
 }
 
 $("#weather-form").on('submit', showWeather);
@@ -35,6 +40,13 @@ function orderMelons(evt) {
         function (result) {
             // TODO: show the result message after your form
             // TODO: if the result code is ERROR, make it show up in red (see our CSS!)
+            var status = result['code'];
+            if (status === "ERROR"){
+                $("#order-status").addClass("order-error");
+            }else{
+                $("#order-status").removeClass("order-error");
+            }
+            $("#order-status").text(result['msg']);
         }
     );
 }
